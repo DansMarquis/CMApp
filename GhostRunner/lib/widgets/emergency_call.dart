a53a3e9ec8aad7a65689fe43e09c372e360e7ef4
Daniel Marques <danielfmarques@ua.pt>
2020-04-01 09:55:29 +0100

    changes

---
+++ GhostRunner/lib/widgets/emergency_call.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:rider/services/firebase_analytics.dart';
+import 'package:rider/utils/colors.dart';
+import 'package:rider/utils/text_styles.dart';
+import 'package:rider/utils/ui_helpers.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+void showEmergencyPopup(BuildContext context) {
+  showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(10.0))),
+          title: Text(
+            'Emergency Helpline',
+            style: isThemeCurrentlyDark(context)
+                ? TitleStyles.white
+                : TitleStyles.black,
+          ),
+          content: Text(
+            'You can call Emergency Services or the RTO of India to file a complaint in case of accident, rude behaviour of drivers, etc.',
+            style: isThemeCurrentlyDark(context)
+                ? BodyStyles.white
+                : BodyStyles.black,
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Cancel'),
+              textColor: invertColorsStrong(context),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(5.0))),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            RaisedButton(
+              child: Text('RTO'),
+              color: MyColors.primary,
+              textColor: MyColors.accent,
+              elevation: 3.0,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(5.0))),
+              onPressed: () {
+                Navigator.pop(context);
+                launch('tel:1800220110');
+                logAnalyticsEvent('rto_click');
+              },
+            ),
+            RaisedButton(
+              child: Text('SOS'),
+              color: MaterialColors.red,
+              textColor: MyColors.white,
+              elevation: 3.0,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(5.0))),
+              onPressed: () {
+                Navigator.pop(context);
+                launch('tel:112');
+                logAnalyticsEvent('sos_click');
+              },
+            ),
+          ],
+        );
+      });
+}
